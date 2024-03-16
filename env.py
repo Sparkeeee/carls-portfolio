746641957b0337cf1f1acfa490b40052b949d1e3
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-03-16 09:39:06 +0000

    setup complete with conect to neon db and cloudinary

---
+++ env.py
@@ -0,0 +1,5 @@
+import os 
+
+os.environ.setdefault('DEBUG', '1')
+os.environ.setdefault('CLOUDINARY_URL', 'cloudinary://484452946645795:3Tv1SlKOo40ATfmasKuF81DJIzg@djmswpr8s')
+os.environ.setdefault('DB_URL', 'postgresql://Porti-data_owner:bxy2WR0NUqCc@ep-frosty-heart-a2h68tz6.eu-central-1.aws.neon.tech/Porti-data?sslmode=require')
